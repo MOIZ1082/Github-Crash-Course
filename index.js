10bfd178f76ba0c4e7197ccbb9959e28db0ae989
Moiz Abid <moizabid1082@gmail.com>
2025-05-14 02:11:28 -0700

    Added isAbove18 function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const age = 19;
-const name = 'papa';
+const name1 = 'papa';
 const dob = '05/09/2005';
 
 function sayName() {
-  console.log('my name is ' + name);
+  console.log('my name is ' + name1);
 }
 
+function isAbove18() {
+  return age >= 18;
+}
